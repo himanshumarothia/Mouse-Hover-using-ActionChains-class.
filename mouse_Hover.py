6edb3a6df42b511c993dc70c55b7dfdc0f6ca55d
Himanshu Marothia <74856577+himanshumarothia@users.noreply.github.com>
2025-09-18 15:54:12 +0530

    Add files via upload

---
+++ mouse_Hover.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+import time 
+
+driver = webdriver.Firefox()
+url  = "https://demo.automationtesting.in/Register.html"
+driver.get(url)
+
+time.sleep(3)
+
+action = ActionChains(driver)                   
+
+hover_element = driver.find_element(By.XPATH,"//a[normalize-space()='SwitchTo']")
+
+action.move_to_element(hover_element).perform()
+time.sleep(2)
+driver.find_element(By.XPATH,"//a[normalize-space()='Frames']").click()
