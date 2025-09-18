654ed13f0b819e67c3d2264996af353dacf68f3b
Himanshu Marothia <74856577+himanshumarothia@users.noreply.github.com>
2025-09-18 16:08:28 +0530

    Update mouse_Hover.py

--- mouse_Hover.py
+++ mouse_Hover.py
@@ -15,4 +15,8 @@ hover_element = driver.find_element(By.XPATH,"//a[normalize-space()='SwitchTo']"
 
 action.move_to_element(hover_element).perform()
 time.sleep(2)
+
 driver.find_element(By.XPATH,"//a[normalize-space()='Frames']").click()
+
+time.sleep(2)
+driver.quit()
